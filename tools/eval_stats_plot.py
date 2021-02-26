51fa7db35d528068176ba3a24109a5bf1592a9d9
RKHashmani <hashmani.raheem@metu.edu.tr>
2021-02-27 00:51:35 +0300

    Update Plotter

--- tools/eval_stats_plot.py
+++ tools/eval_stats_plot.py
@@ -2,28 +2,34 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np
 
-with open('log_validation.csv', 'r') as f:
-    reader = csv.reader(f, delimiter=',')  
+with open('eval_stats/log_validation_classic.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    classical = np.array(list(reader)).astype(float)[:70, :]
+
+with open('eval_stats/log_validation_quantum_depth_1.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=',')
     quantum = np.array(list(reader)).astype(float)
 
-with open('log_validation_classic.csv', 'r') as f:
-    reader = csv.reader(f, delimiter=',')  
-    classical = np.array(list(reader)).astype(float)[:6,:]
+with open('eval_stats/log_validation_quantum_depth_2.csv', 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    quantum2 = np.array(list(reader)).astype(float)  # [:6,:]
 
-plt.plot(np.arange(0, (classical.shape[0]+0.2-1)/5, 0.2), classical[:,1], label='classical')
-plt.plot(np.arange(0, (quantum.shape[0]+0.2-1)/5, 0.2), quantum[:,1], color='purple', label='quantum')
+plt.plot(np.arange(0, (classical.shape[0] + 0.2 - 1) / 5, 0.2), classical[:, 1], label='classical')
+plt.plot(np.arange(0, (quantum.shape[0] + 0.2 - 1) / 5, 0.2), quantum[:, 1], color='purple', label='quantum-1')
+plt.plot(np.arange(0, (quantum2.shape[0] + 0.2 - 1) / 5, 0.2), quantum2[:, 1], color='orange', label='quantum-2')
 plt.title('Validation Loss')
-plt.xlabel('epochs')
+plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 plt.grid()
 plt.savefig('validation_loss.png', dpi=600)
 
 plt.clf()
-plt.plot(np.arange(0, (classical.shape[0]+0.2-1)/5, 0.2), classical[:,0], label='classical')
-plt.plot(np.arange(0, (quantum.shape[0]+0.2-1)/5, 0.2), quantum[:,0], color='purple', label='quantum')
+plt.plot(np.arange(0, (classical.shape[0] + 0.2 - 1) / 5, 0.2), classical[:, 0], label='classical')
+plt.plot(np.arange(0, (quantum.shape[0] + 0.2 - 1) / 5, 0.2), quantum[:, 0], color='purple', label='quantum-1')
+plt.plot(np.arange(0, (quantum2.shape[0] + 0.2 - 1) / 5, 0.2), quantum2[:, 0], color='orange', label='quantum-2')
 plt.title('Validation Accuracy')
-plt.xlabel('epochs')
+plt.xlabel('Epochs')
 plt.ylabel('Accuracy (%)')
 plt.legend()
 plt.grid()
