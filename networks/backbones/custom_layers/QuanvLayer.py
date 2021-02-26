e169a5d8379e7178d80f5b041a708ea8e6e46640
RKHashmani <hashmani.raheem@metu.edu.tr>
2021-02-27 00:19:22 +0300

    Add Support for Batch Size > 1

--- networks/backbones/custom_layers/QuanvLayer.py
+++ networks/backbones/custom_layers/QuanvLayer.py
@@ -3,12 +3,12 @@ import torch
 import pennylane as qml
 from pennylane import numpy as np
 from networks.backbones.custom_layers import remote_cirq
+# import pennylane_qulacs
 
 
 class Quanv(nn.Module):
     def __init__(self, kernal_size, output_depth, circuit_layers=1):
         super().__init__()
-
 
         self.kernal_size = kernal_size  # kernel_size
         self.f = output_depth  # depth
@@ -23,7 +23,7 @@ class Quanv(nn.Module):
             # more circuit layers will be beneficial for this mode
             self.n_qubits = self.f
 
-        self.circuit_layers = circuit_layers # circuit layers
+        self.circuit_layers = circuit_layers  # circuit layers
 
         if (floq_key is not None) and (26 <= self.n_qubits <= 32):  # If it meets the requirement for Floq, use it.
             print("Using Floq as PennyLane Device")
@@ -37,7 +37,7 @@ class Quanv(nn.Module):
         else:
             print("Using Local Hardware as PennyLane Device")
             dev = qml.device("default.qubit", wires=self.n_qubits)
-
+            # dev = qml.device("qulacs.simulator", wires=self.n_qubits)
 
         @qml.qnode(dev)
         def circuit(inputs, weights):
@@ -46,26 +46,28 @@ class Quanv(nn.Module):
             # Apply Hadamard to rest of the qubits, if there are less inputs then n_qubits
             # Hadamard is applied to kill the preference of starting from 0.
             for j in range(self.n_qubits - inputs.shape[0]):
-                qml.Hadamard(wires=j+inputs.shape[0])
+                qml.Hadamard(wires=j + inputs.shape[0])
             # Random Layers is generally bad, they don't train good.
             # We can use Entangling Layers, or our own layers, or layers that we can run on real hardware :)
-            #qml.templates.RandomLayers(weights, wires=list(range(self.n_qubits)))
+            # qml.templates.RandomLayers(weights, wires=list(range(self.n_qubits)))
             qml.templates.layers.BasicEntanglerLayers(weights, wires=list(range(self.n_qubits)))
             return [qml.expval(qml.PauliZ(j)) for j in range(self.f)]
 
-        params = {"weights": (self.circuit_layers, self.n_qubits)} # 4 for area of kernel, 2x2
+        params = {"weights": (self.circuit_layers, self.n_qubits)}  # 4 for area of kernel, 2x2
         self.qlayer = qml.qnn.TorchLayer(circuit, params)
 
-
     def forward(self, x):
-        q_out = torch.zeros((x.shape[3] - self.kernal_size + 1), (x.shape[3] - self.kernal_size + 1), self.f)
+        q_out = torch.zeros(x.shape[0], (x.shape[3] - self.kernal_size + 1), (x.shape[3] - self.kernal_size + 1),
+                            self.f)
+        for idw in range(x.shape[0]):
+            for idx in range(x.shape[3] - self.kernal_size + 1):
+                for idy in range(x.shape[2] - self.kernal_size + 1):
+                    for idz in range(x.shape[1]):
+                        q_out[idw, idx, idy] += self.qlayer(
+                            self.flatten(x[idw, idz, idx:idx + self.kernal_size, idy:idy + self.kernal_size]))
 
-        for idx in range(x.shape[3] - self.kernal_size + 1):
-            for idy in range(x.shape[2] - self.kernal_size + 1):
-                for idz in range(x.shape[1]):
-                    q_out[idx, idy] += self.qlayer(self.flatten(x[0, idz, idx:idx + self.kernal_size, idy:idy + self.kernal_size]))
-
-        return torch.reshape(q_out, (1, self.f, x.shape[3] - self.kernal_size + 1, x.shape[3] - self.kernal_size + 1))
+        return torch.reshape(q_out,
+                             (x.shape[0], self.f, x.shape[3] - self.kernal_size + 1, x.shape[3] - self.kernal_size + 1))
 
     def flatten(self, t):
         t = t.reshape(1, -1)
